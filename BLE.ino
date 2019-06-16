6640373e8a6226b69a833b1d9a0d0ccc96d86483
Jorge Gomez <syvic@sindormir.net>
2019-06-17 00:08:55 +0200

    Avances pero seguimos sin poder recibir datos del 9B

---
+++ BLE.ino
@@ -0,0 +1,112 @@
+
+void BLEscan(){
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+}
+
+
+static void notifyCallbackTX(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+  Serial.print("Notify callback for characteristic TX ");
+  Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+  Serial.print(" of data length ");
+  Serial.println(length);
+  Serial.print("data: ");
+  Serial.println((char*)pData);
+}
+
+
+static void notifyCallbackRX(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+  Serial.print("Notify callback for characteristic RX ");
+  Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+  Serial.print(" of data length ");
+  Serial.println(length);
+  Serial.print("data: ");
+  Serial.println((char*)pData);
+}
+
+
+
+bool connectToServer() {
+  Serial.print("Forming a connection to ");
+  Serial.println(myDevice->getAddress().toString().c_str());
+
+  BLEClient*  pClient  = BLEDevice::createClient();
+  Serial.println(" - Created client");
+
+  pClient->setClientCallbacks(new MyClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  Serial.println(" - Connected to server");
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+  // Obtain a reference to the TX characteristic in the service of the remote BLE server.
+  //pRemoteCharacteristicRX = pRemoteService->getCharacteristic(charUUID_RX);
+  
+  /*
+  if (pRemoteCharacteristicRX == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(charUUID_RX.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our characteristic: RX");
+
+  // Read the value of the characteristic.
+  if (pRemoteCharacteristicRX->canRead()) {
+    std::string value = pRemoteCharacteristicRX->readValue();
+    Serial.print("The characteristic value was: ");
+    Serial.println(value.c_str());
+  }
+
+  if (pRemoteCharacteristicRX->canNotify()) {
+    Serial.println("Habilitando notificaciones RX");
+    pRemoteCharacteristicRX->registerForNotify(notifyCallbackRX);
+  }
+*/
+  
+  /*NUEVO*/
+  pRemoteCharacteristicTX = pRemoteService->getCharacteristic(charUUID_TX);
+
+  if (pRemoteCharacteristicTX == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(charUUID_TX.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our characteristic: TX");
+
+  // Read the value of the characteristic.
+  if (pRemoteCharacteristicTX->canRead()) {
+    std::string value2 = pRemoteCharacteristicTX->readValue();
+    Serial.print("The characteristic value was: ");
+    Serial.println(value2.c_str());
+  }
+
+  if (pRemoteCharacteristicTX->canNotify()) {
+    Serial.println("Habilitando notificaciones TX");
+    pRemoteCharacteristicTX->registerForNotify(notifyCallbackTX);
+  }
+
+  /*NUEVO*/
+
+
+  connected = true;
+
+}
---
+++ keyboard.ino
@@ -0,0 +1,74 @@
+#define PASO_LETRAS 10
+#define INTERVALO_LECTURA 200
+
+
+void keyboard_check() {
+  static unsigned long t_keyboard = 0;
+  uint8_t churro_apaga_led[] =      {0x5A, 0xA5, 0x02, 0x3E, 0x20, 0x02, 0x7D, 0x00, 0x00, 0x20, 0xFF};
+  uint8_t churro_enciende_led[] =   {0x5A, 0xA5, 0x02, 0x3E, 0x20, 0x02, 0x7D, 0x02, 0x00, 0x1E, 0xFF};
+  uint8_t churro_bloquea_patin[] =  {0x5A, 0xA5, 0x02, 0x3E, 0x20, 0x02, 0x70, 0x01, 0x00, 0x2C, 0xFF};
+  uint8_t churro_desbloquea_patin[] = {0x5A, 0xA5, 0x02, 0x3E, 0x20, 0x02, 0x71, 0x01, 0x00, 0x2B, 0xFF};
+
+  if (millis() - t_keyboard > INTERVALO_LECTURA) {
+    t_keyboard = millis();
+
+    //Desbloquear patín
+    if (GO.BtnStart.isPressed() == 1) {
+      pRemoteCharacteristicRX->writeValue(churro_desbloquea_patin, 11, false);
+      GO.lcd.setCursor(170, 0);
+      GO.lcd.setTextFont(4);
+      GO.lcd.setTextColor(RED);
+      GO.lcd.println("LOCK: OFF");
+      GO.Speaker.tone(7000, 10);
+    }
+
+    //Bloquear patín
+    if (GO.BtnSelect.isPressed() == 1) {
+      pRemoteCharacteristicRX->writeValue(churro_bloquea_patin, 11, false);
+      GO.lcd.setCursor(170, 0);
+      GO.lcd.setTextFont(4);
+      GO.lcd.setTextColor(GREEN);
+      GO.lcd.println("LOCK: ON");
+      GO.Speaker.tone(7000, 10);
+    }
+
+    //Encender TAIL
+    if (GO.BtnA.isPressed() == 1) {
+      pRemoteCharacteristicRX->writeValue(churro_enciende_led, 11, false);
+      GO.lcd.setCursor(25, 0);
+      GO.lcd.setTextFont(4);
+      GO.lcd.setTextColor(RED);
+      GO.lcd.println("TAIL: ON");
+      GO.Speaker.tone(5000, 10);
+    }
+
+    //Apagar TAIL
+    if (GO.BtnB.isPressed() == 1) {
+      pRemoteCharacteristicRX->writeValue(churro_apaga_led, 11, false);
+      GO.lcd.setCursor(25, 0);
+      GO.lcd.setTextFont(4);
+      GO.lcd.setTextColor(GREEN);
+      GO.lcd.println("TAIL: OFF");
+      GO.Speaker.tone(4000, 10);
+    }
+
+    if (GO.JOY_X.isAxisPressed() == 2) { //Tecla izquierda
+      GO.Speaker.tone(300, 10);
+    }
+
+    if (GO.JOY_X.isAxisPressed() == 1) { //Tecla derecha
+      GO.Speaker.tone(300, 10);
+    }
+
+    if (GO.JOY_Y.isAxisPressed() == 2) { //Tecla arriba
+      GO.Speaker.tone(200, 5);
+    }
+
+    if (GO.JOY_Y.isAxisPressed() == 1) { //Tecla abajo
+      GO.Speaker.tone(200, 5);
+    }
+
+  }
+  GO.lcd.setTextFont(1);
+  GO.lcd.setTextColor(WHITE);
+}
